3d6a5cc39196c3144fe71293293699b283c1455a
Carlos <carlos.castilla009@gmail.com>
2020-05-05 16:47:21 -0500

    Conexion MySQL con Customer de ejemplo

---
+++ PetCare/Persistence/Config/CustomerConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetCare.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetCare.Persistence.Config
+{
+    public class CustomerConfig
+    {
+        public CustomerConfig(EntityTypeBuilder<Customer> entityBuilder)
+        {
+            entityBuilder.Property(x => x.FirstName).IsRequired().HasMaxLength(30);
+            entityBuilder.Property(x => x.LastName).IsRequired().HasMaxLength(30);
+
+
+        }
+
+    }
+}
---
+++ PetCare/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetCare.Domain.Models;
+using PetCare.Persistence.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetCare.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            new CustomerConfig(builder.Entity<Customer>());
+
+        }
+
+
+
+    }
+}
