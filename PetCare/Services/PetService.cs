02584652bd6463f56626bbc56ad65fd34b76d451
lassdvd <joaquin8708777@outlook.com>
2020-05-08 00:04:06 -0500

    editar_pet

--- PetCare/Services/PetService.cs
+++ PetCare/Services/PetService.cs
@@ -51,6 +51,11 @@ namespace PetCare.Services
                 return new PetResponse("Category not found");
 
             existingpet.Name = pet.Name;
+            existingpet.Name = pet.Name; 
+            existingpet.Age = pet.Age; 
+            existingpet.Breed = pet.Breed; 
+            existingpet.Photo = pet.Photo; 
+            existingpet.Sex = pet.Sex;
 
             try
             {
